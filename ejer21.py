ebd697f022a6249607a859eceacc2e17135ec1dc
Oscar0000000 <oscaramcort13@gmail.com>
2025-10-18 18:26:38 +0200

    Add files via upload

---
+++ ejer21.py
@@ -0,0 +1,9 @@
+x = 1
+n =int(input("Introduce el número hasta el que quieres contar: "))
+while True:
+    if x < n:
+        print (x,end=",")
+    else:
+        break
+    x += 1
+print(x,end=".")
